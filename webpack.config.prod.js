e38bc0b265246d4969d5cfe16252434437aaf47c
Sairam Tagline <sairam.tagline@gmail.com>
2023-01-04 19:14:49 +0530

    prod file changes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,59 +3,58 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 
 const deps = require("./package.json").dependencies;
 module.exports = {
-    output: {
-      publicPath: "https://mc-header.pages.dev"
-    },
+  output: {
+    publicPath: "https://mc-header.pages.dev",
+  },
 
-    resolve: {
-      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.m?js/,
-          type: "javascript/auto",
-          resolve: {
-            fullySpecified: false,
-          },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
         },
-        {
-          test: /\.(css|s[ac]ss)$/i,
-          use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-        {
-          test: /\.(ts|tsx|js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-      ],
-    },
-
-    plugins: [
-      new ModuleFederationPlugin({
-        name: "mc_header",
-        filename: "remoteEntry.js",
-        remotes: {},
-        exposes: {
-          "./App": "./src/App.jsx",
-        },
-        shared: {
-          ...deps,
-          react: {
-            singleton: true,
-            requiredVersion: deps.react,
-          },
-          "react-dom": {
-            singleton: true,
-            requiredVersion: deps["react-dom"],
-          },
-        },
-      }),
-      new HtmlWebPackPlugin({
-        template: "./src/index.html",
-      }),
+      },
     ],
-  };
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "mc_header",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./App": "./src/App.jsx",
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
